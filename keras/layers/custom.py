fc33b1fab92146404bcbee5ca83f0158281edefd
Brian <brian.c.mcmahan@gmail.com>
2016-04-20 19:04:22 -0400

    fixing some of my layers

--- keras/layers/custom.py
+++ keras/layers/custom.py
@@ -22,13 +22,13 @@ def get_shape(x):
 def make_safe(x):
     return K.clip(x, K.common._EPSILON, 1.0 - K.common._EPSILON)
 
-class ProbabilityTensor(Layer):
+class ProbabilityTensor(Wrapper):
     """ function for turning 3d tensor to 2d probability matrix """
     def __init__(self, dense_function=None, *args, **kwargs):
-        self.input_spec = [InputSpec(ndim=3)]
-        self.p_func = dense_function or Dense(1)
         self.supports_masking = True
-        super(ProbabilityTensor, self).__init__(*args, **kwargs)
+        self.input_spec = [InputSpec(ndim=3)]
+        layer = Distribute(dense_function) or Distribute(Dense(1))
+        super(ProbabilityTensor, self).__init__(layer, *args, **kwargs)
 
     def get_output_shape_for(self, input_shape):
         # b,n,f -> b,n 
@@ -57,7 +57,7 @@ class ProbabilityTensor(Layer):
     def call(self, x, mask=None):
         if isinstance(x, list):
             x, mask = x[0], K.not_equal(x[1], 0)
-        energy = K.squeeze(Distribute(self.p_func)(x), 2)
+        energy = K.squeeze(self.layer(x), 2)
         p_matrix = softmax(energy)
         if mask is not None:
             mask = self.squash_mask(mask)
@@ -158,6 +158,38 @@ class Mergecast(Layer):
         return dict(list(base_config.items()) + list(config.items()))   
 
 
+class ExplicitMask(Layer):
+    def __init__(self, mask_tensor, mask_value=0., **kwargs):
+        self.supports_masking = True
+        self.mask_value = mask_value
+        self.mask_tensor = mask_tensor
+        super(ExplicitMask, self).__init__(**kwargs)
+
+    def compute_mask(self, input, input_mask=None):
+        mask = K.not_equal(self.mask_tensor, self.mask_value)
+        if K.ndim(input) == K.ndim(self.mask_tensor):
+            return mask
+        elif K.ndim(input) == K.ndim(self.mask_tensor) + 1:
+            return K.expand_dims(mask)
+        else:
+            return None
+
+    def call(self, x, mask=None):
+        boolean_mask = self.compute_mask(x)
+        return x * K.cast(boolean_mask, K.floatx())
+
+    def get_config(self):
+        config = {'mask_value': self.mask_value}
+        base_config = super(ExplicitMask, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+class LoseMask(Layer):
+    def compute_mask(self, x, mask=None):
+        return None
+
+    def call(self, x, mask=None):
+        return x
+
 class MultiEmbedding(Layer):
     """ function for embedding with encoding """
     def __init__(self,  bit_info, mask_zero=False, *args, **kwargs):
@@ -184,7 +216,8 @@ class MultiEmbedding(Layer):
         self.constraints = {}
         self.input_spec = [InputSpec(shape=I) for I in input_shapes]
         for layer, shape in zip(self.layers, input_shapes):
-            layer.build(shape)
+            if not layer.built:
+                layer.build(shape)
 
         for layer in self.layers:
             self.trainable_weights +=  getattr(layer, 'trainable_weights', [])
@@ -192,13 +225,6 @@ class MultiEmbedding(Layer):
             self.updates  += getattr(layer, 'updates', [])
             self.regularizers  += getattr(layer, 'regularizers', [])
             self.constraints.update(getattr(layer, 'constraints', {}))
-
-    #def get_weights(self):
-    #    weights = [weight for layer in self.layers for weight in layer.get_weights()]
-    #    return weights
-
-    #def set_weights(self, weights):
-    #    self.layer.set_weights(weights)
 
     def get_output_shape_for(self, input_shapes):
         return input_shapes[0] + (self.output_size,)
@@ -212,63 +238,18 @@ class MultiEmbedding(Layer):
             return None
         else:
             return K.not_equal(K.expand_dims(x[0]), 0)
-            #for x_i in x[1:]:
-            #    out.append(K.not_equal(K.expand_dims(x_i), 0))
-            #return K.concatenate([K.not_equal(x_i, 0) for x_i in x], axis=-1)
     
     def get_config(self):
         config = {'output_size': self.output_size}
         base_config = super(MultiEmbedding, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
-class CustomReshape(Reshape):
-    def __init__(self, *args, **kwargs):
-        super(CustomReshape, self).__init__(*args, **kwargs)
-        self.supports_masking = True
-
-    def get_output_shape_for(self, input_shape):
-        return self._fix_unknown_dimension(input_shape, self.target_shape)
-
-    def get_shape(self, x):
-        # In case the target shape is not fully defined,
-        # we need access to the shape of x.
-        # solution:
-        # 1) rely on x._keras_shape
-        # 2) fallback: K.int_shape
-        target_shape = self.target_shape
-        if -1 in target_shape:
-            # target shape not fully defined
-            input_shape = None
-            if hasattr(x, '_keras_shape'):
-                input_shape = x._keras_shape
-            elif hasattr(K, 'int_shape'):
-                input_shape = K.int_shape(x)
-            if input_shape is not None:
-                target_shape = self.get_output_shape_for(input_shape)
-        return target_shape
-
-    def call(self, x, mask=None):
-        #target_shape = get_shape(x)
-        return K.reshape(x, self.target_shape)
-
-    def compute_mask(self, x, mask=None):
-        if mask is None:
-            return mask
-        else:
-            mask_shape = self.target_shape[:-1] + (1,)
-            return K.reshape(mask, mask_shape)
-
-class Summarize(Layer):
+class Summarize(Wrapper):
     #def __init__(self, summary_space_size, *args, **kwargs):
     #    self.summary_space_size = summary_space_size
     def __init__(self, summarizer, *args, **kwargs):
-        super(Summarize, self).__init__(*args, **kwargs)
-        #self.F_rnn = LSTM(summary_space_size, name=self.name+"_rnn_func", unroll=unroll)
         self.supports_masking = True
-        #self.return_mask = return_mask
-        #self.layer = LSTM(summary_space_size, unroll=unroll)
-        self.layer = summarizer
-
+        super(Summarize, self).__init__(summarizer, *args, **kwargs)
 
     def build(self, input_shape=None):
         '''Assumes that self.layer is already set.
@@ -292,7 +273,8 @@ class Summarize(Layer):
 
 
         child_input_shape = (np.prod(input_shape[:-2]),) + input_shape[-2:]
-        self.layer.build(child_input_shape)
+        if not self.layer.built:
+            self.layer.build(child_input_shape)
 
         self.trainable_weights = getattr(self.layer, 'trainable_weights', [])
         self.non_trainable_weights = getattr(self.layer, 'non_trainable_weights', [])
@@ -329,58 +311,3 @@ class Summarize(Layer):
         config = {}  #'summary_space_size': self.summary_space_size
         base_config = super(Summarize, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
-"""
-def Summarize(space_size):
-    def func(layer):
-        sh = K.shape(layer)
-        out_sh = 
-        return Reshape(out_sh)(LSTM(space_size)(Reshape(rnn_sh)(layer)))
-    return func
-class Summarize(Lambda):
-    \""" run an LSTM over the last 2 layers \"""
-    def __init__(self, space_size, *args, **kwargs):
-        self.space_size = space_size
-        f = self.make() 
-        import pdb
-        pdb.set_trace()
-        super(Summarize, self).__init__(self, f, *args, **kwargs)
-
-    def make(self):
-        def func(layer):
-            sh = K.shape(layer)
-            rnn_sh = (np.prod(sh[:-2]), ) + sh[-2:]
-            out_sh = sh[:-2] + (self.space_size, )
-            return Reshape(out_sh)(LSTM(self.space_size)(Reshape(rnn_sh)(layer)))
-        return func
-
-    def get_output_shape_for(self, input_shape):
-        if isinstance(input_shape, (tuple, list)):
-            raise Exception("Not prepared for this right now")
-
-        return input_shape[:-2] + (self.space_size,)
-
-    def compress_expand(self, shape):
-        head, tail = shape[:-2], shape[-2:]
-        c_shape = (np.prod(head),) + tail
-        e_shape = head + (self.summary_space_size,)
-        return (CustomReshape(c_shape, name=self.name+"_compress"), 
-                CustomReshape(e_shape, name=self.name+"_expand"))
-        #return Reshape(c_shape), Reshape(e_shape)
-        #return Reshape((-1,)+shape[-2:]), Reshape((-1,self.summary_space_size))
-
-
-
-        input_shape = get_shape(x) 
-        c_shape = (np.prod(input_shape[:-2]),) + input_shape[-2:]
-        e_shape = input_shape[:-2] + (self.summary_space_size, )
-        F_compress, F_expand = self.compress_expand(input_shape)
-        if mask is not None:
-            mask = K.reshape(mask, c_shape[:-1]+(1,))
-        #return K.reshape(self.F_rnn(K.reshape(x, c_shape), mask), e_shape)
-        out = F_compress(x)
-        out = self.F_rnn(out)
-        out = F_expand(out)
-        return out
-        
-"""
